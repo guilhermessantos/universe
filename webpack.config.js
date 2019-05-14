0aa964c2b6e04fcecfb714e83134c75c98b095a8
Guilherme Souza <zguilhermesouza@gmail.com>
2019-05-14 12:35:34 -0300

    add vue and dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
 const path = require('path')
 const sass = require('node-sass')
 const sassUtils = require('node-sass-utils')(sass)
-const sassVars = require(__dirname + '/src/assets/javascripts/variables.js');
+const sassVars = require(__dirname + '/src/assets/javascripts/variables.js')
+
+const VueLoaderPlugin      = require('vue-loader/lib/plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
       {
         test: /\.scss$/,
         use: [
@@ -46,8 +52,15 @@ module.exports = {
     ]
   },
   plugins: [
+    new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
       filename: 'style.css'
-    }),
-  ]
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+    open: true,
+    port: 8000
+  }
 }
