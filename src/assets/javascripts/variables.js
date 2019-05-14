eb3e308f93a6b9c1182518458e2ba3f4ed6363c4
Guilherme Souza <zguilhermesouza@gmail.com>
2019-05-14 09:20:54 -0300

    add theme

--- src/assets/javascripts/variables.js
+++ src/assets/javascripts/variables.js
@@ -53,6 +53,19 @@ module.exports = {
     'link': '#f8f8f8'
   },
 
+  theme: {
+    'default': {
+      'base': '#4d538c',
+      'medium': '#474c81',
+      'dark': '#3b406c'
+    },
+    'red': {
+      'base': '#d60057',
+      'medium': '#c50050',
+      'dark': '#a40043'
+    }
+  },
+
   display: [
     'none',
     'block',
