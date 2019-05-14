5383d0fb3c86b0fcf8f44d30fe1c5d6bbb06e39d
Guilherme Souza <zguilhermesouza@gmail.com>
2019-05-13 21:56:59 -0300

    add style border

---
+++ src/assets/javascripts/variables.js
@@ -0,0 +1,32 @@
+module.exports = {
+  config: {
+    prefix: 'ui-'
+  },
+
+  border: {
+    prefix: 'b',
+    position: {
+      default: 'default',
+      top: 't',
+      right: 'r',
+      bottom: 'b',
+      left: 'l'
+    },
+    size: {
+      small: '1px',
+      medium: '3px',
+      large: '5px'
+    }
+  },
+
+  padding: {
+    position: {
+      top: 't',
+      right: 'r'
+    },
+    size: {
+      small: '10px',
+      medium: '30px'
+    }
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const sass = require('node-sass')
+const sassUtils = require('node-sass-utils')(sass)
+const sassVars = require(__dirname + '/src/assets/javascripts/variables.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
@@ -13,9 +16,31 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
+          {
+            loader: "style-loader"
+          },
           MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              functions: {
+                'get($keys)': function(keys) {
+                  keys = keys.getValue().split('.');
+                  let result = sassVars;
+
+                  for (let i = 0; i < keys.length; i++) {
+                    result = result[keys[i]];
+                  }
+
+                  result = sassUtils.castToSass(result);
+                  return result;
+                }
+              }
+            }
+          }
         ]
       }
     ]
