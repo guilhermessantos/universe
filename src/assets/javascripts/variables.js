604690543f5da3b994ec9df6659bfbcc89434913
Guilherme Souza <zguilhermesouza@gmail.com>
2019-05-13 22:09:52 -0300

    add color

--- src/assets/javascripts/variables.js
+++ src/assets/javascripts/variables.js
@@ -1,32 +1,41 @@
 module.exports = {
   config: {
-    prefix: 'ui-'
+    'prefix': 'ui-',
+    'prefix-modifier': 'is-'
   },
 
   border: {
-    prefix: 'b',
-    position: {
-      default: 'default',
-      top: 't',
-      right: 'r',
-      bottom: 'b',
-      left: 'l'
+    'prefix': 'b',
+    'position': {
+      'default': 'default',
+      'top': 't',
+      'right': 'r',
+      'bottom': 'b',
+      'left': 'l'
     },
     size: {
-      small: '1px',
-      medium: '3px',
-      large: '5px'
+      'small': '1px',
+      'medium': '3px',
+      'large': '5px'
     }
   },
 
   padding: {
-    position: {
-      top: 't',
-      right: 'r'
+    'position': {
+      'top': 't',
+      'right': 'r'
     },
-    size: {
-      small: '10px',
-      medium: '30px'
+    'size': {
+      'small': '10px',
+      'medium': '30px'
     }
+  },
+
+  color: {
+    'gray-100': '#fafafa',
+    'gray-200': '#E5E5E5',
+    'black': '#000000',
+    'white': '#ffffff',
+    'link': '#f8f8f8'
   }
 }
