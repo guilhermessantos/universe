0ec8c0af61223e04610eceecdf26bdf2ef144a79
Guilherme Souza <zguilhermesouza@gmail.com>
2019-08-07 08:53:52 -0300

    add RemovePlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,16 @@
 const common = require('./webpack.common')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const RemovePlugin = require('remove-files-webpack-plugin')
+
+function recursiveIssuer(m) {
+  if (m.issuer) {
+    return recursiveIssuer(m.issuer);
+  } else if (m.name) {
+    return m.name;
+  } else {
+    return false;
+  }
+}
 
 common.sassLoader.use.unshift(
   { loader: MiniCssExtractPlugin.loader }
@@ -7,13 +18,58 @@ common.sassLoader.use.unshift(
 
 module.exports = {
   mode: 'production',
-  entry: common.entry,
+
+  entry: {
+    main: './src/main.js',
+    style: './src/assets/sass/style.scss',
+    vue: './src/assets/sass/vue.scss'
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        scssStyles: {
+          name: 'style',
+          test: (m, c, entry = 'style') =>
+            m.constructor.name === 'CssModule' && recursiveIssuer(m) === entry,
+          chunks: 'all',
+          enforce: true,
+        },
+        vueStyles: {
+          name: 'vue',
+          test: (m, c, entry = 'vue') =>
+            m.constructor.name === 'CssModule' && recursiveIssuer(m) === entry,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
 
   plugins: common.plugins.concat([
     new MiniCssExtractPlugin({
-      filename: 'style.css',
-      chunkFilename: 'style.css'
+      filename: '[name].css',
+      chunkFilename: '[name].css'
     }),
+    new RemovePlugin({
+      after: {
+        exclude: ['dist/main.js'],
+        test: [
+          {
+            folder: 'dist',
+            method: (filePath) => {
+                return new RegExp(/\.js$/, 'm').test(filePath);
+            }
+          },
+          {
+            folder: 'dist',
+            method: (filePath) => {
+                return new RegExp(/\/main.css$/, 'm').test(filePath);
+            }
+          }
+        ]
+    }
+    })
   ]),
 
   module: {
@@ -21,7 +77,5 @@ module.exports = {
       common.sassLoader,
       common.vueLoader
     ]
-  },
-
-  output: common.output
+  }
 }
