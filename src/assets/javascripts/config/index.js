88443950340b9708e2f51190afce3af810c08c7e
Guilherme Souza <zguilhermesouza@gmail.com>
2019-12-16 22:32:50 -0300

    main styles

---
+++ src/assets/javascripts/config/index.js
@@ -0,0 +1,27 @@
+const { borderCSS } = require('./border')
+const { colorCSS } = require('./color')
+const { display } = require('./display')
+const { flex } = require('./flex')
+const { headingCSS } = require('./heading')
+const { marginCSS } = require('./margin')
+const { paddingCSS } = require('./padding')
+const { paletteCSS } = require('./palette')
+const { roundedCSS } = require('./rounded')
+const { sizes } = require('./sizes')
+const { textCSS } = require('./text')
+
+const style = {
+  borderCSS,
+  colorCSS,
+  display,
+  flex,
+  headingCSS,
+  marginCSS,
+  paddingCSS,
+  paletteCSS,
+  roundedCSS,
+  sizes,
+  textCSS
+}
+
+module.exports = style
