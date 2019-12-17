ee4c0011c013438a34c7f29aaf5d5445aef25e0d
Guilherme Souza <zguilhermesouza@gmail.com>
2019-12-16 22:27:41 -0300

    add border

---
+++ src/assets/javascripts/config/border/index.js
@@ -0,0 +1,33 @@
+const prefix = require('../prefix')
+
+const position = {
+  'default': 'default',
+  'bottom': 'b',
+  'left': 'l',
+  'right': 'r',
+  'top': 't'
+}
+
+const size = {
+  'small': '1px',
+  'medium': '3px',
+  'large': '5px'
+}
+
+const border = {
+  size
+}
+
+const borderCSS = {
+  'prefix-position': prefix.config.border.position,
+  'prefix-size': prefix.config.border.size,
+  position,
+  size
+}
+
+module.exports = { 
+  border,
+  borderCSS,
+  position,
+  size
+}
