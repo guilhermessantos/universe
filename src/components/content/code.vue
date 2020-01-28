65fbae47ca58b65dabfb98e813605b62e98ab112
Guilherme Souza <zguilhermesouza@gmail.com>
2020-01-27 21:01:53 -0300

    removed all files

--- src/components/content/code.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="un-code-wrap">
-    <div class="un-box un-code">
-      <highlight-code lang="html">
-        <slot></slot>
-      </highlight-code>
-    </div>
-  </div>
-</template>
