a6ca8f0f53975d0afb7b54c1c3b4174603aa23d8
Guilherme Souza <zguilhermesouza@gmail.com>
2020-01-29 09:39:23 -0300

    add Container

---
+++ src/components/Container/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 20px;
+  padding-right: 20px;
+  max-width: 1240px;
+`
