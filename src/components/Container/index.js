97011c5c902ecd544a90006658588c7e85b164d2
Guilherme Souza <zguilhermesouza@gmail.com>
2020-02-03 14:33:47 -0300

    add style Content and Example

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -7,3 +7,14 @@ export const Container = styled.section`
   padding-right: 20px;
   max-width: 1240px;
 `
+export const Content = styled.div`
+  align-items: flex-start;
+  display: ${props => props.sidebar && 'flex'};
+  max-width: 900px;
+  margin-left: auto;
+`
+
+export const Example = styled.div`
+  flex: 1;
+  margin-left: 30px;
+`
