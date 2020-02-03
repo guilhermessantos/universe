d8e96736157c6d9cf9c43481a25031f993535217
Guilherme Souza <zguilhermesouza@gmail.com>
2020-02-03 14:34:13 -0300

    add Container Sidebar

---
+++ src/components/ContainerSidebar/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import t from 'prop-types'
+import * as S from './styles'
+
+const ContainerSidebar = ({ data, active, handleMenu }) => (
+  <S.Container>
+    {Object.keys(data).map(item => (
+      <S.Item
+        key={item}
+        active={active === data[item]}
+        onClick={() => handleMenu(data[item])}
+      >
+        <S.Key>{item}</S.Key>
+        <S.Value>{data[item]}</S.Value>
+      </S.Item>
+    ))}
+  </S.Container>
+)
+
+ContainerSidebar.propTypes = {
+  data: t.object.isRequired,
+  active: t.object.isRequired,
+  handleMenu: t.func.isRequired,
+}
+
+export { ContainerSidebar }
---
+++ src/components/ContainerSidebar/styles.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  font-size: 14px;
+  border: 1px solid #42153c;
+  border-radius: 10px;
+  padding: 30px 30px 0;
+  position: relative;
+  width: 270px;
+
+  &::before {
+    background: #ed3947;
+    border-radius: 10px;
+    bottom: -30px;
+    content: '';
+    left: 0;
+    min-width: 100px;
+    position: absolute;
+    top: 30px;
+  }
+`
+
+export const Key = styled.div`
+  min-width: 100px;
+  padding: 15px 0;
+  position: relative;
+
+  &::before {
+    content: '';
+    background: #000;
+    left: -30px;
+    top: 50%;
+    position: absolute;
+    width: 100%;
+    opacity: 0.1;
+    z-index: 3;
+    transform: translateY(-50%);
+    transition: height 0.3s;
+  }
+`
+export const Value = styled.div`
+  border-bottom: 1px solid #42153c;
+  flex: 1;
+  padding: 15px 0;
+  text-align: right;
+  transition: color 0.4s, padding-right 0.4s;
+`
+
+export const Item = styled.div`
+  cursor: pointer;
+  position: relative;
+  display: flex;
+  z-index: 1;
+
+  &::before {
+    content: '';
+    position: absolute;
+    right: -31px;
+    top: 50%;
+    height: ${props => (props.active ? '100%' : 0)};
+    width: 1px;
+    background: #ed3947;
+    transition: height 0.4s;
+    transform: translateY(-50%);
+  }
+
+  &:last-child ${Value} {
+    border: 0;
+  }
+
+  &:hover ${Value} {
+    padding-right: ${props => (props.active ? 0 : '10px')};
+  }
+
+  ${Value} {
+    color: ${props => (props.active ? '#FFFFFFF' : '#BBB8BF')};
+  }
+
+  ${Key}::before {
+    height: ${props => (props.active ? '100%' : 0)};
+  }
+`
