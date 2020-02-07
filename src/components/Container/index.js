c4122d2bb4e6f57b2a0d1d6bb5d80f8f13d87c47
Guilherme Souza <zguilhermesouza@gmail.com>
2020-02-07 09:30:05 -0300

    adjustments in the example and content

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,4 +1,13 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+export const Content = styled.div`
+  align-items: center;
+  display: ${props => props.sidebar && 'flex'};
+  height: 75vh;
+  max-width: 900px;
+  margin-left: auto;
+  padding-top: ${props => !props.sidebar && '70px'};
+`
 
 export const Container = styled.section`
   margin-left: auto;
@@ -6,15 +15,32 @@ export const Container = styled.section`
   padding-left: 20px;
   padding-right: 20px;
   max-width: 1240px;
-`
-export const Content = styled.div`
-  align-items: flex-start;
-  display: ${props => props.sidebar && 'flex'};
-  max-width: 900px;
-  margin-left: auto;
+  height: inherit;
+  overflow: ${props => props.shadow && 'hidden'};
+
+  ${Content} {
+    ${props =>
+      props.shadow &&
+      css`
+        position: relative;
+
+        &::before {
+          background: #0f081e;
+          box-shadow: 0 0 100px 100px #0f081e;
+          content: '';
+          height: 0;
+          position: absolute;
+          bottom: 0;
+          width: 100%;
+          z-index: 2;
+        }
+      `}
+  }
 `
 
 export const Example = styled.div`
   flex: 1;
-  margin-left: 30px;
+  display: ${props => props.center && 'flex'};
+  margin-left: 60px;
+  justify-content: ${props => props.center && 'center'};
 `
