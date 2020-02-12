ec3b184cabeac9b45a633402e23d0ad2c6254158
Guilherme Souza <zguilhermesouza@gmail.com>
2020-02-12 08:57:44 -0300

    add themes

--- config/themes.js
+++ config/themes.js
@@ -9,4 +9,49 @@ export default {
     medium: '#c50050',
     dark: '#a40043',
   },
+  career: {
+    base: '#5e9f09',
+    medium: '#5b9a09',
+    dark: '#4c8008',
+  },
+  money: {
+    base: '#007934',
+    medium: '#007030',
+    dark: '#005d28',
+  },
+  future: {
+    base: '#ff5000',
+    medium: '#eb4a00',
+    dark: '#c43d00',
+  },
+  health: {
+    base: '#00838f',
+    medium: '#007480',
+    dark: '#00626b',
+  },
+  'self-esteem': {
+    base: '#f7323f',
+    medium: '#e42e3a',
+    dark: '#be2630',
+  },
+  house: {
+    base: '#f38b00',
+    medium: '#e08000',
+    dark: '#bb6b00',
+  },
+  spirituality: {
+    base: '#77226c',
+    medium: '#6e1f64',
+    dark: '#5b1a53',
+  },
+  food: {
+    base: '#009ad6',
+    medium: '#0094ce',
+    dark: '#007cab',
+  },
+  family: {
+    base: '#b12470',
+    medium: '#a32167',
+    dark: '#881c56',
+  },
 }
